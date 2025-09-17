0b04e7928666519073db531b3faaaef656ca386a
yashgoel <yash.goel8370@gmail.com>
2025-09-17 14:33:23 +0530

    add comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,6 +87,7 @@ export default function Home() {
         {viewPDF ? (
           <>
             <div className="border-1 border-gray-300 p-5 rounded-lg bg-gray-100 w-[95%] lg:w-[60%]">
+              {/* form */}
               <div className="mx-10 cursor-pointer">
                 <Image
                   onClick={() => setViewPDF(false)}
@@ -95,28 +96,40 @@ export default function Home() {
                   alt="Back"
                 ></Image>
               </div>
+
+              {/* user's name */}
               <div className="w-[95%] lg:w-[50%] flex-1 space-y-5 border-black border-1 m-auto mt-2 bg-white p-5 shadow-md">
                 <div className="flex gap-2">
                   <p className="font-bold">Name:</p>
                   <p className="text-gray-600">{formData.name}</p>
                 </div>
+
+                {/* user's email */}
                 <div className="flex gap-2">
                   <p className="font-bold">Email:</p>
                   <p className="text-gray-600">{formData.email}</p>
                 </div>
+
+                {/* user's phone */}
                 <div className="flex gap-2">
                   <p className="font-bold">Phone:</p>
                   <p className="text-gray-600">{formData.phone}</p>
                 </div>
+
+                {/* user's position */}
                 <div className="flex gap-2">
                   <p className="font-bold">Position:</p>
                   <p className="text-gray-600">{formData.position}</p>
                 </div>
+
+                {/* user's description */}
                 <div className="flex gap-2">
                   <p className="font-bold">Description:</p>
                   <p className="text-gray-600">{formData.description}</p>
                 </div>
               </div>
+
+              {/* download pdf button */}
               <div className="w-[95%] lg:w-[50%] flex-1 space-y-5 m-auto mt-2 shadow-md">
                 <button
                   onClick={downloadPDF}
@@ -144,6 +157,8 @@ export default function Home() {
                   <div className="px-5">
                     <Image src={user} width={25} alt="User" />
                   </div>
+
+                  {/* user's name label and input */}
                   <div className="flex-1">
                     <label htmlFor="name" className="font-semibold">
                       Name
@@ -159,6 +174,8 @@ export default function Home() {
                     />
                   </div>
                 </div>
+
+                {/* user's email label and input */}
 
                 <div className="rounded-xl bg-white shadow-md py-1 flex items-center">
                   <div className="px-5">
@@ -180,6 +197,8 @@ export default function Home() {
                   </div>
                 </div>
 
+                {/* user's phone label and input */}
+
                 <div className="rounded-xl bg-white shadow-md py-1 flex items-center">
                   <div className="px-5">
                     <Image src={phone} width={25} alt="Phone" />
@@ -199,6 +218,8 @@ export default function Home() {
                     />
                   </div>
                 </div>
+
+                {/* user's position label and input */}
 
                 <div className="rounded-xl bg-white shadow-md py-1 flex items-center">
                   <div className="px-5">
@@ -220,6 +241,8 @@ export default function Home() {
                   </div>
                 </div>
 
+                {/* user's description label and input */}
+
                 <div className="rounded-xl bg-white shadow-md py-1 flex items-center">
                   <div className="px-5">
                     <Image src={description} width={25} alt="Description" />
@@ -240,12 +263,17 @@ export default function Home() {
                 </div>
               </div>
               <div className="flex mt-5 gap-4">
+                {/* view pdf button */}
+
                 <button
                   onClick={viewPDFButton}
                   className="py-1 px-3 bg-green-700 text-white font-bold text-lg w-full rounded-md cursor-pointer"
                 >
                   View PDF
                 </button>
+
+                {/* download pdf button */}
+
                 <button
                   onClick={downloadPDF}
                   className="py-1 px-3 bg-green-700 text-white font-bold text-lg w-full rounded-md cursor-pointer flex justify-center items-center"
